92d21c986caf2e5e6366ceb4c63853d8b87063a3
ranamagdi <ranagrida@gmail.com>
2023-09-24 00:08:04 +0300

    card

--- lib/main.dart
+++ lib/main.dart
@@ -25,55 +25,70 @@ class _NavigationExampleState extends State<NavigationExample> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          colorSchemeSeed: const Color(0xff6750a4), useMaterial3: true),
+      home: Scaffold(
 
-      appBar: AppBar(
-        actions: [
-          IconButton(onPressed: (){}, icon: const Icon (Icons.search))
-        ],
+        appBar: AppBar(
+          actions: [
+            IconButton(onPressed: (){}, icon: const Icon (Icons.search))
+          ],
+        ),
+        bottomNavigationBar: NavigationBar(
+          onDestinationSelected: (int index) {
+            setState(() {
+              currentPageIndex = index;
+            });
+          },
+          indicatorColor: Colors.amber[800],
+          selectedIndex: currentPageIndex,
+          destinations: const <Widget>[
+            NavigationDestination(
+              selectedIcon: Icon(Icons.home),
+              icon: Icon(Icons.home_outlined),
+              label: 'Home',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.business),
+              label: 'Business',
+            ),
+            NavigationDestination(
+              selectedIcon: Icon(Icons.school),
+              icon: Icon(Icons.school_outlined),
+              label: 'School',
+            ),
+          ],
+        ),
+        body: <Widget>[
+          Container(
+           child: Card(
+              margin: const EdgeInsets.all(20),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  ListTile(
+                    leading:Image.asset('assets/JhdUiOPP_400x400.png',width: 100,height: 100,),
+                    title: const Text('Demo Title'),
+                    subtitle: const Text('This is a simple card in Flutter.'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.green,
+            alignment: Alignment.center,
+            child: const Text('Page 2'),
+          ),
+          Container(
+            color: Colors.blue,
+            alignment: Alignment.center,
+            child: const Text('Page 3'),
+          ),
+        ][currentPageIndex],
       ),
-      bottomNavigationBar: NavigationBar(
-        onDestinationSelected: (int index) {
-          setState(() {
-            currentPageIndex = index;
-          });
-        },
-        indicatorColor: Colors.amber[800],
-        selectedIndex: currentPageIndex,
-        destinations: const <Widget>[
-          NavigationDestination(
-            selectedIcon: Icon(Icons.home),
-            icon: Icon(Icons.home_outlined),
-            label: 'Home',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.business),
-            label: 'Business',
-          ),
-          NavigationDestination(
-            selectedIcon: Icon(Icons.school),
-            icon: Icon(Icons.school_outlined),
-            label: 'School',
-          ),
-        ],
-      ),
-      body: <Widget>[
-        Container(
-          color: Colors.red,
-          alignment: Alignment.center,
-          child: const Text('Page 1'),
-        ),
-        Container(
-          color: Colors.green,
-          alignment: Alignment.center,
-          child: const Text('Page 2'),
-        ),
-        Container(
-          color: Colors.blue,
-          alignment: Alignment.center,
-          child: const Text('Page 3'),
-        ),
-      ][currentPageIndex],
     );
   }
 }
